cfcf415ff078eb3200cb617aa6c2b84733ff198c
yahel <yahel.fri@gmail.com>
2019-12-08 17:54:54 +0200

    regitration

--- Text-Reading-for-Visually-Impaired/Text-Reading-for-Visually-Impaired/Registration.Designer.cs
+++ Text-Reading-for-Visually-Impaired/Text-Reading-for-Visually-Impaired/Registration.Designer.cs
@@ -44,11 +44,11 @@
             // 
             this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label2.Font = new System.Drawing.Font("Arial", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
             this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label2.Location = new System.Drawing.Point(592, 35);
+            this.label2.Location = new System.Drawing.Point(584, 35);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(273, 91);
+            this.label2.Size = new System.Drawing.Size(268, 89);
             this.label2.TabIndex = 0;
             this.label2.Text = "sign in";
             // 
@@ -56,11 +56,11 @@
             // 
             this.maleRB.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.maleRB.AutoSize = true;
-            this.maleRB.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.maleRB.Font = new System.Drawing.Font("Arial", 22.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
             this.maleRB.ForeColor = System.Drawing.Color.White;
             this.maleRB.Location = new System.Drawing.Point(921, 441);
             this.maleRB.Name = "maleRB";
-            this.maleRB.Size = new System.Drawing.Size(176, 67);
+            this.maleRB.Size = new System.Drawing.Size(187, 70);
             this.maleRB.TabIndex = 1;
             this.maleRB.TabStop = true;
             this.maleRB.Text = "male";
@@ -71,11 +71,11 @@
             this.firstNameTB.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.firstNameTB.BackColor = System.Drawing.Color.MidnightBlue;
             this.firstNameTB.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.firstNameTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.firstNameTB.Font = new System.Drawing.Font("Arial", 22.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
             this.firstNameTB.ForeColor = System.Drawing.Color.DarkGray;
             this.firstNameTB.Location = new System.Drawing.Point(231, 198);
             this.firstNameTB.Name = "firstNameTB";
-            this.firstNameTB.Size = new System.Drawing.Size(367, 61);
+            this.firstNameTB.Size = new System.Drawing.Size(367, 68);
             this.firstNameTB.TabIndex = 2;
             this.firstNameTB.Text = "first name";
             this.firstNameTB.Click += new System.EventHandler(this.textBox_clicked);
@@ -85,10 +85,10 @@
             // 
             this.OKBT.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.OKBT.BackColor = System.Drawing.Color.LightSteelBlue;
-            this.OKBT.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.OKBT.Font = new System.Drawing.Font("Arial", 22.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
             this.OKBT.Location = new System.Drawing.Point(789, 625);
             this.OKBT.Name = "OKBT";
-            this.OKBT.Size = new System.Drawing.Size(183, 102);
+            this.OKBT.Size = new System.Drawing.Size(200, 102);
             this.OKBT.TabIndex = 3;
             this.OKBT.Text = "OK";
             this.OKBT.UseVisualStyleBackColor = false;
@@ -99,11 +99,11 @@
             this.lastNameTB.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.lastNameTB.BackColor = System.Drawing.Color.MidnightBlue;
             this.lastNameTB.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.lastNameTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.lastNameTB.Font = new System.Drawing.Font("Arial", 22.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
             this.lastNameTB.ForeColor = System.Drawing.Color.DarkGray;
             this.lastNameTB.Location = new System.Drawing.Point(851, 198);
             this.lastNameTB.Name = "lastNameTB";
-            this.lastNameTB.Size = new System.Drawing.Size(367, 61);
+            this.lastNameTB.Size = new System.Drawing.Size(367, 68);
             this.lastNameTB.TabIndex = 4;
             this.lastNameTB.Text = "last name";
             this.lastNameTB.Click += new System.EventHandler(this.textBox_clicked);
@@ -114,11 +114,11 @@
             this.userNameTB.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.userNameTB.BackColor = System.Drawing.Color.MidnightBlue;
             this.userNameTB.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.userNameTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.userNameTB.Font = new System.Drawing.Font("Arial", 22.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
             this.userNameTB.ForeColor = System.Drawing.Color.DarkGray;
             this.userNameTB.Location = new System.Drawing.Point(231, 343);
             this.userNameTB.Name = "userNameTB";
-            this.userNameTB.Size = new System.Drawing.Size(367, 61);
+            this.userNameTB.Size = new System.Drawing.Size(367, 68);
             this.userNameTB.TabIndex = 5;
             this.userNameTB.Text = "user name";
             this.userNameTB.Click += new System.EventHandler(this.textBox_clicked);
@@ -129,11 +129,11 @@
             this.passwordTB.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.passwordTB.BackColor = System.Drawing.Color.MidnightBlue;
             this.passwordTB.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.passwordTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.passwordTB.Font = new System.Drawing.Font("Arial", 22.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
             this.passwordTB.ForeColor = System.Drawing.Color.DarkGray;
             this.passwordTB.Location = new System.Drawing.Point(851, 343);
             this.passwordTB.Name = "passwordTB";
-            this.passwordTB.Size = new System.Drawing.Size(367, 61);
+            this.passwordTB.Size = new System.Drawing.Size(367, 68);
             this.passwordTB.TabIndex = 6;
             this.passwordTB.Text = "password";
             this.passwordTB.Click += new System.EventHandler(this.textBox_clicked);
@@ -143,10 +143,10 @@
             // 
             this.cancelBT.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.cancelBT.BackColor = System.Drawing.Color.LightSteelBlue;
-            this.cancelBT.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
-            this.cancelBT.Location = new System.Drawing.Point(462, 622);
+            this.cancelBT.Font = new System.Drawing.Font("Arial", 22.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.cancelBT.Location = new System.Drawing.Point(432, 624);
             this.cancelBT.Name = "cancelBT";
-            this.cancelBT.Size = new System.Drawing.Size(202, 105);
+            this.cancelBT.Size = new System.Drawing.Size(222, 105);
             this.cancelBT.TabIndex = 7;
             this.cancelBT.Text = "Cancel";
             this.cancelBT.UseVisualStyleBackColor = false;
@@ -156,11 +156,11 @@
             // 
             this.femaleRB.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.femaleRB.AutoSize = true;
-            this.femaleRB.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.femaleRB.Font = new System.Drawing.Font("Arial", 22.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
             this.femaleRB.ForeColor = System.Drawing.Color.White;
             this.femaleRB.Location = new System.Drawing.Point(921, 509);
             this.femaleRB.Name = "femaleRB";
-            this.femaleRB.Size = new System.Drawing.Size(221, 67);
+            this.femaleRB.Size = new System.Drawing.Size(237, 70);
             this.femaleRB.TabIndex = 8;
             this.femaleRB.TabStop = true;
             this.femaleRB.Text = "female";
@@ -171,11 +171,11 @@
             this.EmailTB.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.EmailTB.BackColor = System.Drawing.Color.MidnightBlue;
             this.EmailTB.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.EmailTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.EmailTB.Font = new System.Drawing.Font("Arial", 22.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
             this.EmailTB.ForeColor = System.Drawing.Color.DarkGray;
             this.EmailTB.Location = new System.Drawing.Point(231, 476);
             this.EmailTB.Name = "EmailTB";
-            this.EmailTB.Size = new System.Drawing.Size(367, 61);
+            this.EmailTB.Size = new System.Drawing.Size(367, 68);
             this.EmailTB.TabIndex = 9;
             this.EmailTB.Text = "E-mail";
             this.EmailTB.Click += new System.EventHandler(this.textBox_clicked);
