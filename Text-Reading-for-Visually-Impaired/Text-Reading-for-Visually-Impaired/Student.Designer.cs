3745ac64aeac12df9c2d26d7ef213c18dbee6892
victorzele96 <57448124+victorzele96@users.noreply.github.com>
2020-01-01 19:48:06 +0200

    start work on update student info , and add forms and back buttons

--- Text-Reading-for-Visually-Impaired/Text-Reading-for-Visually-Impaired/Student.Designer.cs
+++ Text-Reading-for-Visually-Impaired/Text-Reading-for-Visually-Impaired/Student.Designer.cs
@@ -28,57 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.colorDialog1 = new System.Windows.Forms.ColorDialog();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             this.colorDialog2 = new System.Windows.Forms.ColorDialog();
             this.colorDialog3 = new System.Windows.Forms.ColorDialog();
             this.button4 = new System.Windows.Forms.Button();
             this.colorDialog4 = new System.Windows.Forms.ColorDialog();
             this.colorDialog5 = new System.Windows.Forms.ColorDialog();
             this.thems = new System.Windows.Forms.ComboBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
-            this.button1.Location = new System.Drawing.Point(563, 177);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(381, 85);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Choose form color";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
-            this.button2.Location = new System.Drawing.Point(563, 66);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(381, 85);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Choose Button color";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
-            this.button3.Location = new System.Drawing.Point(1046, 66);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(381, 85);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Choose font color";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
             this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
-            this.button4.Location = new System.Drawing.Point(1046, 631);
+            this.button4.Location = new System.Drawing.Point(705, 462);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(381, 85);
+            this.button4.Size = new System.Drawing.Size(314, 87);
             this.button4.TabIndex = 3;
             this.button4.Text = "Logout";
             this.button4.UseVisualStyleBackColor = true;
@@ -97,24 +63,45 @@
             "3",
             "4",
             "5"});
-            this.thems.Location = new System.Drawing.Point(30, 66);
+            this.thems.Location = new System.Drawing.Point(162, 180);
             this.thems.Name = "thems";
-            this.thems.Size = new System.Drawing.Size(271, 59);
+            this.thems.Size = new System.Drawing.Size(314, 59);
             this.thems.TabIndex = 0;
             this.thems.Text = "Pick  Theme";
             this.thems.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged_1);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.button5.Location = new System.Drawing.Point(162, 461);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(314, 87);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Choose a story";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.button6.Location = new System.Drawing.Point(705, 172);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(314, 87);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Update student Information";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // Student
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1557, 769);
+            this.ClientSize = new System.Drawing.Size(1204, 737);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.thems);
             this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
             this.Name = "Student";
             this.Text = "Student";
             this.Load += new System.EventHandler(this.Student_Load);
@@ -123,16 +110,14 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ColorDialog colorDialog1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.ColorDialog colorDialog2;
         private System.Windows.Forms.ColorDialog colorDialog3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.ColorDialog colorDialog4;
         private System.Windows.Forms.ColorDialog colorDialog5;
         private System.Windows.Forms.ComboBox thems;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
     }
 }
--- Text-Reading-for-Visually-Impaired/Text-Reading-for-Visually-Impaired/Student.cs
+++ Text-Reading-for-Visually-Impaired/Text-Reading-for-Visually-Impaired/Student.cs
@@ -17,36 +17,6 @@ namespace Text_Reading_for_Visually_Impaired
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            if(colorDialog1.ShowDialog() != System.Windows.Forms.DialogResult.Cancel)
-            {
-                Student.ActiveForm.BackColor = colorDialog1.Color;
-            }
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            if (colorDialog2.ShowDialog() != System.Windows.Forms.DialogResult.Cancel)
-            {
-                button1.BackColor = colorDialog2.Color;
-                button2.BackColor = colorDialog2.Color;
-                button3.BackColor = colorDialog2.Color;
-                button4.BackColor = colorDialog2.Color;
-            }
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            if (colorDialog3.ShowDialog() != System.Windows.Forms.DialogResult.Cancel)
-            {
-                button1.ForeColor = colorDialog3.Color;
-                button2.ForeColor = colorDialog3.Color;
-                button3.ForeColor = colorDialog3.Color;
-                button4.ForeColor = colorDialog3.Color;
-            }
-        }
-
         private void button4_Click(object sender, EventArgs e)
         {
             this.Hide();
@@ -57,21 +27,103 @@ namespace Text_Reading_for_Visually_Impaired
 
         private void Student_Load(object sender, EventArgs e)
         {
-
-        }
-
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-        }
-
-        private void groupBox1_Enter(object sender, EventArgs e)
-        {
-
         }
 
         private void comboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
         {
+            if (thems.SelectedItem == "Default")
+            {
+                Student.ActiveForm.BackColor = Color.White;//change form color
 
+                button4.ForeColor = Color.Black;           //change font color
+                button5.ForeColor = Color.Black;
+                button6.ForeColor = Color.Black;
+
+                button4.BackColor = Color.Gainsboro;       //change back color buttons
+                button5.BackColor = Color.Gainsboro;
+                button6.BackColor = Color.Gainsboro;
+            }
+
+            if (thems.SelectedItem == "1")
+            {
+                Student.ActiveForm.BackColor = Color.Black;//change form color
+
+                button4.ForeColor = Color.Yellow;           //change font color
+                button5.ForeColor = Color.Yellow;
+                button6.ForeColor = Color.Yellow;
+
+                button4.BackColor = Color.Black;       //change back color buttons
+                button5.BackColor = Color.Black;
+                button6.BackColor = Color.Black;
+            }
+
+            if (thems.SelectedItem == "2")
+            {
+                Student.ActiveForm.BackColor = Color.Gainsboro;//change form color
+
+                button4.ForeColor = Color.Red;           //change font color
+                button5.ForeColor = Color.Red;
+                button6.ForeColor = Color.Red;
+
+                button4.BackColor = Color.Black;       //change back color buttons
+                button5.BackColor = Color.Black;
+                button6.BackColor = Color.Black;
+            }
+
+            if (thems.SelectedItem == "3")
+            {
+                Student.ActiveForm.BackColor = Color.Black;//change form color
+
+                button4.ForeColor = Color.Yellow;           //change font color
+                button5.ForeColor = Color.Yellow;
+                button6.ForeColor = Color.Yellow;
+
+                button4.BackColor = Color.Black;       //change back color buttons
+                button5.BackColor = Color.Black;
+                button6.BackColor = Color.Black;
+            }
+
+            if (thems.SelectedItem == "4")
+            {
+                Student.ActiveForm.BackColor = Color.Black;//change form color
+
+                button4.ForeColor = Color.Yellow;           //change font color
+                button5.ForeColor = Color.Yellow;
+                button6.ForeColor = Color.Yellow;
+
+                button4.BackColor = Color.Black;       //change back color buttons
+                button5.BackColor = Color.Black;
+                button6.BackColor = Color.Black;
+            }
+
+            if (thems.SelectedItem == "5")
+            {
+                Student.ActiveForm.BackColor = Color.Black;//change form color
+
+                button4.ForeColor = Color.Yellow;           //change font color
+                button5.ForeColor = Color.Yellow;
+                button6.ForeColor = Color.Yellow;
+
+                button4.BackColor = Color.Black;       //change back color buttons
+                button5.BackColor = Color.Black;
+                button6.BackColor = Color.Black;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Stories story = new Stories();
+            story.ShowDialog();
+            this.Close();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Update_student_Information student_info = new Update_student_Information();
+            student_info.ShowDialog();
+            this.Close();
         }
     }
 }
