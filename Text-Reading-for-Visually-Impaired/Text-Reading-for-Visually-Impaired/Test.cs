74bf1dcf89f87dcc42bd2bc8e347244836e4b443
victorzele96 <57448124+victorzele96@users.noreply.github.com>
2020-01-03 16:20:25 +0200

    add arie changes
    building again test drop box inheritance to student windows

---
+++ Text-Reading-for-Visually-Impaired/Text-Reading-for-Visually-Impaired/Test.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Text_Reading_for_Visually_Impaired
+{
+    public partial class Test : Form
+    {
+        private bool isCollapsed;
+        public Test()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (isCollapsed)
+            {
+                panelDropDown.Height += 10;
+                if (panelDropDown.Size == panelDropDown.MaximumSize)
+                {
+                    timer1.Stop();
+                    isCollapsed = false;
+                }
+            }
+            else
+            {
+                panelDropDown.Height -= 10;
+                if (panelDropDown.Size == panelDropDown.MinimumSize)
+                {
+                    timer1.Stop();
+                    isCollapsed = true;
+                }
+            }
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void Test_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
