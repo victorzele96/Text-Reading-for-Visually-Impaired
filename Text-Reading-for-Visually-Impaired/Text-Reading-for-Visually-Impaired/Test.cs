b58e6a00344709ce188239bf16ae3e1445151a63
yahel <yahel.fri@gmail.com>
2020-01-04 12:47:21 +0200

    font size in second page

--- Text-Reading-for-Visually-Impaired/Text-Reading-for-Visually-Impaired/Test.cs
+++ Text-Reading-for-Visually-Impaired/Text-Reading-for-Visually-Impaired/Test.cs
@@ -105,6 +105,7 @@ namespace Text_Reading_for_Visually_Impaired
                 }
                 else if(obj is Label || obj is TextBox || obj is RichTextBox || obj is Button)
                 {
+                    
                     obj.Font = new Font(obj.Font.FontFamily, obj.Font.Size - 2f, obj.Font.Style);
                 }
                 /*else
