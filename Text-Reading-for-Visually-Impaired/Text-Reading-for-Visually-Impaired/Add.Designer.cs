4a0c696c647eefce3c1a5ae1fd7270a96088433c
Arie <57365299+jayzpkz@users.noreply.github.com>
2020-01-13 21:13:31 +0200

    add window - > add questions button

--- Text-Reading-for-Visually-Impaired/Text-Reading-for-Visually-Impaired/Add.Designer.cs
+++ Text-Reading-for-Visually-Impaired/Text-Reading-for-Visually-Impaired/Add.Designer.cs
@@ -28,101 +28,126 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Add));
-            this.back = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.story_name = new System.Windows.Forms.Label();
-            this.ok = new System.Windows.Forms.Button();
+            this.bacBTN = new System.Windows.Forms.Button();
+            this.Add_storyBTN = new System.Windows.Forms.Button();
+            this.story_nameTB = new System.Windows.Forms.TextBox();
+            this.story_nameLBL = new System.Windows.Forms.Label();
+            this.okBTN = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.UpdatesRTB = new System.Windows.Forms.RichTextBox();
+            this.Add_questionBTN = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // back
+            // bacBTN
             // 
-            this.back.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.back.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
-            this.back.Location = new System.Drawing.Point(322, 476);
-            this.back.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.back.Name = "back";
-            this.back.Size = new System.Drawing.Size(180, 73);
-            this.back.TabIndex = 0;
-            this.back.Text = "Back";
-            this.back.UseVisualStyleBackColor = true;
-            this.back.Click += new System.EventHandler(this.button1_Click);
+            this.bacBTN.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.bacBTN.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.bacBTN.Location = new System.Drawing.Point(1367, 542);
+            this.bacBTN.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.bacBTN.Name = "bacBTN";
+            this.bacBTN.Size = new System.Drawing.Size(196, 85);
+            this.bacBTN.TabIndex = 0;
+            this.bacBTN.Text = "Back";
+            this.bacBTN.UseVisualStyleBackColor = true;
+            this.bacBTN.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // Add_storyBTN
             // 
-            this.button2.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.button2.BackColor = System.Drawing.Color.Green;
-            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 23F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.ForeColor = System.Drawing.Color.White;
-            this.button2.Location = new System.Drawing.Point(102, 160);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(400, 255);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Add File";
-            this.button2.TextAlign = System.Drawing.ContentAlignment.BottomRight;
-            this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.Add_storyBTN.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Add_storyBTN.BackColor = System.Drawing.SystemColors.Control;
+            this.Add_storyBTN.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Add_storyBTN.ForeColor = System.Drawing.Color.Black;
+            this.Add_storyBTN.Location = new System.Drawing.Point(893, 429);
+            this.Add_storyBTN.Name = "Add_storyBTN";
+            this.Add_storyBTN.Size = new System.Drawing.Size(196, 85);
+            this.Add_storyBTN.TabIndex = 1;
+            this.Add_storyBTN.Text = "Add Story";
+            this.Add_storyBTN.UseVisualStyleBackColor = false;
+            this.Add_storyBTN.Click += new System.EventHandler(this.button2_Click);
             // 
-            // textBox1
+            // story_nameTB
             // 
-            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 23F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(338, 51);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(234, 60);
-            this.textBox1.TabIndex = 2;
+            this.story_nameTB.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.story_nameTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.story_nameTB.Location = new System.Drawing.Point(764, 65);
+            this.story_nameTB.Name = "story_nameTB";
+            this.story_nameTB.Size = new System.Drawing.Size(309, 35);
+            this.story_nameTB.TabIndex = 2;
             // 
-            // story_name
+            // story_nameLBL
             // 
-            this.story_name.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.story_name.AutoSize = true;
-            this.story_name.Font = new System.Drawing.Font("Microsoft Sans Serif", 23F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.story_name.Location = new System.Drawing.Point(27, 51);
-            this.story_name.Name = "story_name";
-            this.story_name.Size = new System.Drawing.Size(290, 53);
-            this.story_name.TabIndex = 3;
-            this.story_name.Text = "Story Name:";
+            this.story_nameLBL.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.story_nameLBL.AutoSize = true;
+            this.story_nameLBL.Font = new System.Drawing.Font("Microsoft Sans Serif", 23F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.story_nameLBL.Location = new System.Drawing.Point(425, 54);
+            this.story_nameLBL.Name = "story_nameLBL";
+            this.story_nameLBL.Size = new System.Drawing.Size(290, 53);
+            this.story_nameLBL.TabIndex = 3;
+            this.story_nameLBL.Text = "Story Name:";
             // 
-            // ok
+            // okBTN
             // 
-            this.ok.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.ok.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ok.Location = new System.Drawing.Point(102, 476);
-            this.ok.Name = "ok";
-            this.ok.Size = new System.Drawing.Size(180, 73);
-            this.ok.TabIndex = 4;
-            this.ok.Text = "OK";
-            this.ok.UseVisualStyleBackColor = true;
+            this.okBTN.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.okBTN.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.okBTN.Location = new System.Drawing.Point(532, 542);
+            this.okBTN.Name = "okBTN";
+            this.okBTN.Size = new System.Drawing.Size(196, 85);
+            this.okBTN.TabIndex = 4;
+            this.okBTN.Text = "OK";
+            this.okBTN.UseVisualStyleBackColor = true;
+            this.okBTN.Click += new System.EventHandler(this.ok_Click);
             // 
             // panel1
             // 
             this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.panel1.Controls.Add(this.textBox1);
-            this.panel1.Controls.Add(this.story_name);
-            this.panel1.Controls.Add(this.ok);
-            this.panel1.Controls.Add(this.back);
-            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.Add_questionBTN);
+            this.panel1.Controls.Add(this.UpdatesRTB);
+            this.panel1.Controls.Add(this.story_nameTB);
+            this.panel1.Controls.Add(this.story_nameLBL);
+            this.panel1.Controls.Add(this.okBTN);
+            this.panel1.Controls.Add(this.bacBTN);
+            this.panel1.Controls.Add(this.Add_storyBTN);
             this.panel1.Location = new System.Drawing.Point(3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(595, 578);
+            this.panel1.Size = new System.Drawing.Size(1573, 639);
             this.panel1.TabIndex = 5;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // UpdatesRTB
+            // 
+            this.UpdatesRTB.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.UpdatesRTB.Location = new System.Drawing.Point(532, 150);
+            this.UpdatesRTB.Name = "UpdatesRTB";
+            this.UpdatesRTB.Size = new System.Drawing.Size(541, 247);
+            this.UpdatesRTB.TabIndex = 5;
+            this.UpdatesRTB.Text = "";
+            // 
+            // Add_questionBTN
+            // 
+            this.Add_questionBTN.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Add_questionBTN.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Add_questionBTN.Location = new System.Drawing.Point(893, 542);
+            this.Add_questionBTN.Name = "Add_questionBTN";
+            this.Add_questionBTN.Size = new System.Drawing.Size(196, 85);
+            this.Add_questionBTN.TabIndex = 6;
+            this.Add_questionBTN.Text = "Add Questions";
+            this.Add_questionBTN.UseVisualStyleBackColor = true;
+            this.Add_questionBTN.Click += new System.EventHandler(this.Add_questionBTN_Click);
             // 
             // Add
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Silver;
-            this.ClientSize = new System.Drawing.Size(600, 583);
+            this.ClientSize = new System.Drawing.Size(1578, 644);
             this.Controls.Add(this.panel1);
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Add";
             this.Text = "Add";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Add_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
@@ -132,11 +157,13 @@
 
         #endregion
 
-        private System.Windows.Forms.Button back;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label story_name;
-        private System.Windows.Forms.Button ok;
+        private System.Windows.Forms.Button bacBTN;
+        private System.Windows.Forms.Button Add_storyBTN;
+        private System.Windows.Forms.TextBox story_nameTB;
+        private System.Windows.Forms.Label story_nameLBL;
+        private System.Windows.Forms.Button okBTN;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RichTextBox UpdatesRTB;
+        private System.Windows.Forms.Button Add_questionBTN;
     }
 }
--- Text-Reading-for-Visually-Impaired/Text-Reading-for-Visually-Impaired/Add.cs
+++ Text-Reading-for-Visually-Impaired/Text-Reading-for-Visually-Impaired/Add.cs
@@ -8,12 +8,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Text_Reading_for_Visually_Impaired
 {
     public partial class Add : Form
     {
-        private OleDbConnection connection = new OleDbConnection();
         public Admin Admin_main;
         public Add(Admin main)
         {
@@ -25,7 +25,6 @@ namespace Text_Reading_for_Visually_Impaired
         {
             this.Hide();
             Admin_main.Show();
-            //this.Close();
         }
 
         private void Add_Load(object sender, EventArgs e)
@@ -39,8 +38,37 @@ namespace Text_Reading_for_Visually_Impaired
 
             if(openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
-                string str_file_name = openFileDialog1.FileName;
-                MessageBox.Show(str_file_name);  //prints the file path
+                string str_file_path = openFileDialog1.FileName;
+                MessageBox.Show(str_file_path);  //prints the file path
+
+                string readText = File.ReadAllText(str_file_path);
+                MessageBox.Show(readText);
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void ok_Click(object sender, EventArgs e)
+        {
+            Admin_main.Show();
+            this.Hide();
+        }
+
+        private void Add_questionBTN_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string str_file_path = openFileDialog1.FileName;
+                MessageBox.Show(str_file_path);  //prints the file path
+
+                string readText = File.ReadAllText(str_file_path);
+                //MessageBox.Show(readText);
+
             }
         }
     }
