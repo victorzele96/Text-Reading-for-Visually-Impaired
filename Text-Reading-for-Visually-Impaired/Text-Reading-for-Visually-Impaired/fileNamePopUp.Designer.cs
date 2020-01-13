744a66c704ad845aa29678b54ea753b1da4605e4
yahel <yahel.fri@gmail.com>
2020-01-13 19:36:00 +0200

    story name existence check

--- Text-Reading-for-Visually-Impaired/Text-Reading-for-Visually-Impaired/fileNamePopUp.Designer.cs
+++ Text-Reading-for-Visually-Impaired/Text-Reading-for-Visually-Impaired/fileNamePopUp.Designer.cs
@@ -28,12 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label1.Location = new System.Drawing.Point(77, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(759, 81);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "please choose a name for  youre story";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.textBox1.Location = new System.Drawing.Point(244, 165);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(440, 53);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.button1.Location = new System.Drawing.Point(503, 477);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(181, 87);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.button2.Location = new System.Drawing.Point(258, 477);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(181, 87);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "cancel";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // fileNamePopUp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(922, 655);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "fileNamePopUp";
             this.Text = "fileNamePopUp";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- Text-Reading-for-Visually-Impaired/Text-Reading-for-Visually-Impaired/fileNamePopUp.cs
+++ Text-Reading-for-Visually-Impaired/Text-Reading-for-Visually-Impaired/fileNamePopUp.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,47 @@ namespace Text_Reading_for_Visually_Impaired
 {
     public partial class fileNamePopUp : Form
     {
-        public fileNamePopUp()
+        public second_Page myMain;
+        public fileNamePopUp(second_Page myMain)
         {
             InitializeComponent();
+            this.myMain = myMain;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        public Boolean check_Story_Name_Existence(String name)
+        {         
+            string fileName = "Database11.accdb";
+            string path = Path.Combine(Environment.CurrentDirectory, @"Data\", fileName);
+            string workingDirectory = Environment.CurrentDirectory;
+            String path2 = Directory.GetParent(workingDirectory).Parent.FullName + "\\Database11.accdb";
+            string connStr = String.Format(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0}", path2);
+            string query = " SELECT * FROM stories";
+            using (OleDbConnection conn = new OleDbConnection(connStr))
+            {
+                conn.Open();
+                OleDbCommand cmd = new OleDbCommand(query, conn);
+                OleDbDataReader reader = cmd.ExecuteReader();
+                try
+                {
+                    while (reader.Read())
+                    {
+                        if (reader[3].ToString()==name)
+                        {
+                            return true;
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+                return false;
+            }
         }
     }
 }
