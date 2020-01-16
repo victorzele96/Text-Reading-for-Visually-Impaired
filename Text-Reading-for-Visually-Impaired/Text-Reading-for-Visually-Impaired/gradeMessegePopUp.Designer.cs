36a7fbbdb5957fc637aa9a7d3b63d4f9a5638239
yahelfri <49039385+yahelfri@users.noreply.github.com>
2020-01-16 23:17:28 +0200

    grade design

--- Text-Reading-for-Visually-Impaired/Text-Reading-for-Visually-Impaired/gradeMessegePopUp.Designer.cs
+++ Text-Reading-for-Visually-Impaired/Text-Reading-for-Visually-Impaired/gradeMessegePopUp.Designer.cs
@@ -123,6 +123,7 @@
             this.Controls.Add(this.pictureBox1);
             this.Name = "gradeMessegePopUp";
             this.Text = "gradeMessegePopUp";
+            this.Load += new System.EventHandler(this.gradeMessegePopUp_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
