f7c38099996fb7569b23895ee6f0467ac7b3624f
victorzele96 <57448124+victorzele96@users.noreply.github.com>
2020-01-17 01:14:24 +0200

    update the  student progression window form

--- Text-Reading-for-Visually-Impaired/Text-Reading-for-Visually-Impaired/Student Progression.Designer.cs
+++ Text-Reading-for-Visually-Impaired/Text-Reading-for-Visually-Impaired/Student Progression.Designer.cs
@@ -28,19 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(36, 70);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(944, 337);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.button1.Location = new System.Drawing.Point(1114, 515);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(189, 70);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Back";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Student_Progression
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1582, 653);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "Student_Progression";
             this.Text = "Student_Progression";
+            this.Load += new System.EventHandler(this.Student_Progression_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
     }
 }
--- Text-Reading-for-Visually-Impaired/Text-Reading-for-Visually-Impaired/Student Progression.cs
+++ Text-Reading-for-Visually-Impaired/Text-Reading-for-Visually-Impaired/Student Progression.cs
@@ -12,9 +12,37 @@ namespace Text_Reading_for_Visually_Impaired
 {
     public partial class Student_Progression : Form
     {
-        public Student_Progression()
+        public bool isAdmin;
+        Reports admin_main;
+        Teacher teacher_main;
+        public Student_Progression(Reports main)
         {
             InitializeComponent();
+            admin_main = main;
+            isAdmin = true;
+        }
+        public Student_Progression(Teacher main)
+        {
+            InitializeComponent();
+            teacher_main = main;
+            isAdmin = false;
+        }
+
+        private void Student_Progression_Load(object sender, EventArgs e)
+        {
+            Database11DataSetTableAdapters.ProfileTableAdapter pr = new Database11DataSetTableAdapters.ProfileTableAdapter();
+            Database11DataSet.ProfileDataTable dt2 = pr.GetData();//pr=profile
+            dataGridView1.DataSource = dt2;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (isAdmin)
+                admin_main.Show();
+            else
+                teacher_main.Show();
+
+            this.Hide();
         }
     }
 }
