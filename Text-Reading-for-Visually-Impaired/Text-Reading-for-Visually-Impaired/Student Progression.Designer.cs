e948365bf72c77736ceb01c2fa5003fc26969946
victorzele96 <57448124+victorzele96@users.noreply.github.com>
2020-01-17 01:04:24 +0200

    create new form Student Progression

---
+++ Text-Reading-for-Visually-Impaired/Text-Reading-for-Visually-Impaired/Student Progression.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace Text_Reading_for_Visually_Impaired
+{
+    partial class Student_Progression
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Student_Progression
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1582, 653);
+            this.Name = "Student_Progression";
+            this.Text = "Student_Progression";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
